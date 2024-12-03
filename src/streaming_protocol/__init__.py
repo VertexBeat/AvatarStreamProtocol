254da88a3dfbf8edb90968de2d092206c88a8e72
Dom <d.mowius@icloud.com>
2024-12-03 21:06:06 +0100

    updated versioning

--- src/streaming_protocol/__init__.py
+++ src/streaming_protocol/__init__.py
@@ -1,4 +1,5 @@
 # src/streaming_protocol/__init__.py
+__version__ = "0.2.0"  # Add or update this line
 from streaming_protocol import DataSerializer, BaseMetadata, TimelineMetadata, AudioMetadata
 
 
